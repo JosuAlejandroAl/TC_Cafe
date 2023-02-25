dbad4e729a43ac5a5e5d4067f85693f6f5409c17
JosuAlejandroAl <A01732454@tec.mx>
2023-02-24 23:33:10 -0600

    done with the file os test cases finished

--- cafe.py
+++ cafe.py
@@ -1,49 +1,54 @@
 import re
 
-def drink(beverage_name):
-    beverage_name = beverage_name.replace(" ", "")
-    pattern = re.compile("^[a-zA-Z]{2,15}$")
-    if pattern.match(beverage_name):
+def drink(item_name):
+    item_name = item_name.replace(" ", "")
+    pattern = re.compile("^[a-zA-Z]{2,15}$") #validate only alphabetic characters and minimum of 2 and a maximum of 15 characters
+    if pattern.match(item_name):
         print("Valido nombre de bebida")
     else:
         print("Invalido nombre de bebida")
 
-def size_list(size_string):
+def size_list(size_value):
     size_list = []
     try:
-        # Remove spaces and split the string into a list
-        size_string = size_string.replace(" ", "")
-        size_list = [int(num) for num in size_string.split(",")]
+        # remove blank spaces
+        size_value = size_value.replace(" ", "")
+        size_list = [int(num) for num in size_value.split(",")]
         
-        # Validate the size list
+        # validate the size conditionals
         if len(size_list) < 1 or len(size_list) > 5:
             print("El numero de los tamanos de bebidas debe ser entre 1 y 5")
             return None
+        
         for num in size_list:
+
             if num < 1 or num > 48:
                 print("Los tamanos deben estar en un rango de 1 - 48")
                 return None
+            
         if size_list != sorted(size_list):
             print("Los números deben estar en orden ascendente")
             return None
+        
     except ValueError:
-        print("Invalid size list.")
+        print("invalido los valores de la lista")
         return None
     
     print("El orden de los tamanos es la siguiente", sorted(size_list))
     return sorted(size_list)
 
-# Get input from the user
-input_string = input(" ingresa el nombre de la bebida y despues el tamano")
+# open the file and read the test cases
+with open('test.txt', 'r') as f:
+    input_lines = f.readlines()
 
-# Split the input string into the beverage name and size list
-input_list = input_string.split(",")
-
-# Call the functions to validate the input
-if len(input_list) < 2:
-    print("No ingresaste bien los valores recuerda que debe ser primero el nombre y depsues con una coma ',' sigue los tamanos de la bebida .")
-else:
-    beverage_name = input_list[0]
-    size_string = ",".join(input_list[1:])
-    drink(beverage_name)
-    size_list(size_string)
+for line in input_lines:
+    input_list = line.strip().split(",")
+    
+    # call the functions 
+    if len(input_list) < 2:
+        print("No ingresaste bien los valores recuerda que debe ser primero el nombre de la bebida y despues con una coma ',' sigue los tamanos de la bebida")
+    else:
+        item_name = input_list[0]
+        size_value = ",".join(input_list[1:]) #input list is split using the comma as a separator
+        drink(item_name)
+        size_list(size_value)
